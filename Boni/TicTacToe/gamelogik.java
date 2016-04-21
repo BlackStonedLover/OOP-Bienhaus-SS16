c27941f9523cfc6d369d3f21af2440e51f5bdac9
Konstantin <stonedlover@darkdestinylp.de>
2016-04-21 07:52:00 +0200

    Grundzüge Tic Tac Toe fertig
    Tic Tac Toe bis auf Gewinn Bedingung fertig. (folgt im laufe des Tages)

--- Boni/TicTacToe/gamelogik.java
+++ Boni/TicTacToe/gamelogik.java
@@ -7,4 +7,41 @@ public class gamelogik
             for(int d =0; d < 3; d++)
             Spielbrett[i][d] = ' ';
     }
+    
+    public boolean checkIfIllegal(int row int column)
+    {
+        if(row >2 || column > 2 || row < 0 || column < 0  )
+        return true;
+        if(Spielbrett[column][row] == 'x' || Spielbrett[column][row])
+        return true;
+        
+        return false;
+    }
+    public void  SpielSteinplatzieren(char player, int spalte, int reihe){
+        Spielbrett[spalte][reihe] = player;
+    }
+   public void DisplayBoard(){
+       System.out.println("0 " + Spielbrett[0][0] + " |" + Spielbrett[1][0] + " |" + Spielbrett[2][0] );
+       System.out.println("--|--|--");
+       Systen.out.println("1 " + Spielbrett[0][1] + " |" + Spielbrett[1][1] + " |" + Spielbrett[2][1]);
+       System.out.println("--|--|--");
+       System.out.println("2 " + Spielbrett[0][2] + " |" +Spielbrett[1][2] + " |" + Spielbrett[2][2]);
+   }
+    public boolean CheckForWinner()
+    {
+    return false;    
+    }
+    public boolean CheckForDraw()
+    {
+        return false;
+    }
+    public char SpielerWechsel(char player)
+    {
+       char neuerSpieler = 'e';
+        if(player == 'x')
+        neuerSpieler = 'o';
+        if(player == 'o')
+        neuerSpieler = 'x';
+        return neuerSpieler;
+    }
 }
--- Boni/TicTacToe/start.java
+++ Boni/TicTacToe/start.java
@@ -5,6 +5,28 @@ public class start
     {
         Scanner sc = new Scanner(System.in);
         Game g = new Game();
-        g.iniitGame();
+        g.initGame();
+        
+        char Spieler = 'x';
+        int reihe,spalte;
+        while(true)
+        {
+            System.out.println("Spieler " + Spieler + " ist jetzt an der Reihe: ");
+            System.out.println("Bitte Feld eingeben: ");
+            spalte = sc.nextInt();
+            reihe = sc.nextInt();
+         
+            while(g.checkIfIllegal(reihe, spalte))
+            {
+                System.out.println("Dieser Zug ist ungültig bitte neuen Zug eingeben: ");
+                spalte = sc.nextInt();
+                reihe = sc.nextInt();
+            }
+           g.SpielSteinplatzieren(Spieler, spalte, reihe);
+           g.DisplayBoard();
+           g.CheckForWinner();
+           g.CheckForDraw();
+           g.SpielerWechsel(Spieler);
+        }
     }
 }
